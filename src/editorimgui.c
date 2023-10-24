349db53790673ab1df223c8d63f9ef7cc2983e9d
nnaapp <connor@jnj.org>
2023-10-24 10:36:30 -0500

    Made tile selector only allow one selection, made demo window a keybind (D)

--- src/editorimgui.c
+++ src/editorimgui.c
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+bool TileSelector();
+
 int main(void)
 {
     Vector2 displaySize = { 1024, 1024 };
@@ -27,33 +29,13 @@ int main(void)
         // cImGui and rlImGui start frame
         rlImGuiBegin();
 
-        igShowDemoWindow(NULL);
+        // D for demo window (shows all the things ImGui can do)
+        static bool showDemo = false;
+        if (IsKeyPressed(KEY_D)) showDemo = !showDemo;
+        if (showDemo) igShowDemoWindow(NULL);
 
-        // Testing for tile selector, for tilemap editor
-        bool showWindow = true;
-        igBegin("Tile Selector Window", &showWindow, 0);
-
-        igText("Tile Set Selector");
-
-        static bool selected[9] = {true, false, false, false, false, false, false, false, false};
-
-        for (int y = 0; y < 3; y++)
-        {
-            for (int x = 0; x < 3; x++)
-            {
-                if (x > 0)
-                    igSameLine(0, -10.0f);
-                igPushID_Int(3 * y + x);
-                if (igSelectable_Bool("Tile", selected[3 * y + x], 0, (ImVec2){80, 80}))
-                {
-                    selected[3 * y + x] ^= 1;
-                }
-                igPopID();
-            }
-        }
-
-        igEnd();
-        // End of tile selector testing
+        // Function for prototype tile selector
+        TileSelector();
 
         // cImGui and rlImGui end frame
         rlImGuiEnd();
@@ -66,4 +48,39 @@ int main(void)
 
     // Raylib window shutdown
     CloseWindow();
+}
+
+bool TileSelector()
+{
+    igBegin("Tile Selector Window", NULL, 0);
+
+    igText("Tile Set Selector");
+
+    static bool selected[9] = {true, false, false, false, false, false, false, false, false};
+    static int currentSelect = 0;
+
+    for (int y = 0; y < 3; y++)
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            if (x > 0) igSameLine(0, -10.0f);
+            
+            igPushID_Int(3 * y + x);
+            if (igSelectable_Bool("Tile", selected[3 * y + x], 0, (ImVec2){80, 80}))
+            {
+                
+                selected[3 * y + x] ^= 1;
+                if (selected[3 * y + x]) 
+                { 
+                    selected[currentSelect] = false;
+                    currentSelect = 3 * y + x; 
+                }
+            }
+            igPopID();
+        }
+    }
+
+    igEnd();
+
+    return currentSelect;
 }
